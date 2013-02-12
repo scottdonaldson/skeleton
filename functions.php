ed4ec64b35e5c031a52a8ca57b4ce22b1c5ebb48
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-11 20:26:35 -0600

    jQuery 1.8.3 latest compatible with WP, added thumbnails

--- functions.php
+++ functions.php
@@ -7,10 +7,13 @@ register_nav_menu('primary', 'Primary Menu');
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
-   // As of Nov. 2012, latest jQuery is 1.8.2
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js", false, null);
+   // Latest WP-compatible jQuery is 1.8.3
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js", false, null);
    wp_enqueue_script('jquery');
 }
+
+// Featured images
+add_theme_support('post-thumbnails');
 
 // Remove some stuff from head
 remove_action('wp_head', 'feed_links_extra', 3);
